53746e9532e730f5dfe8c5f8c2ef6f2ce0afe6d5
00680 <121090850+00680@users.noreply.github.com>
2022-12-20 17:59:30 +0800

    Update 兌換序號.js

--- Tampermonkey/兌換序號.js
+++ Tampermonkey/兌換序號.js
@@ -1,9 +1,8 @@
 // ==UserScript==
 // @name         快樂玩自動兌換序號
-// @namespace    http://tampermonkey.net/
 // @version      0.1
 // @description  快樂玩自動兌換序號
-// @author       You
+// @author       00680
 // @match        *https://www.mangot5.com/Index/Billing/couponList*
 // @icon         https://www.google.com/s2/favicons?sz=64&domain=tampermonkey.net
 // @grant        none
