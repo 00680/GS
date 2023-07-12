2aa4f1d41f47933aa15023bfbc659e90ed210173
00680 <121090850+00680@users.noreply.github.com>
2023-07-13 01:20:15 +0800

    Create 快樂玩驗證碼.user.js

---
+++ Tampermonkey/快樂玩驗證碼/快樂玩驗證碼.user.js
@@ -0,0 +1,21 @@
+// ==UserScript==
+// @name         快樂玩驗證碼
+// @namespace    GS tam
+// @version      0.1
+// @description  快樂玩驗證碼
+// @author       00680
+// @match        https://www.mangot5.com/Index/Member/Login*
+// @require      https://raw.githubusercontent.com/jiangts/JS-OTP/master/dist/jsOTP-es5.min.js
+// @grant        none
+// ==/UserScript==
+
+(function() {
+    'use strict';
+    const codeField = document.getElementById('validCode');
+    codeField.type = 'password';
+    codeField.maxLength = 16;
+    codeField.addEventListener("change", () => {
+        const totp = new jsOTP.totp();
+        codeField.value = totp.getOtp(codeField.value);
+    })
+})();
